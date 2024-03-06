bcba0a4c7d30a1cbde81d90b3b85588ada5e5c2c
Richard Mabb <richardmabb@hotmail.com>
2024-03-06 21:02:28 +0000

    cleaner code for milestone.py

--- milestone_2.py
+++ milestone_2.py
@@ -1,9 +1,29 @@
 import random
+
+def select_random_word(word_list):
+    """Selects a random word from the given list."""
+    return random.choice(word_list)
+
+def prompt_user_for_guess():
+    """Prompts the user to enter a single letter."""
+    return input("Enter a single letter: ")
+
+def validate_guess(guess):
+    """Validates the user's guess."""
+    if len(guess) == 1 and guess.isalpha():
+        print("Good guess!")
+    else:
+        print("Oops! That is not a valid input.")
+
+# List of words for guessing
 word_list = ["apple", "grapefruit", "orange", "mango", "passionfruit"]
-word = random.choice(word_list)
+
+# Select a random word from the list
+word = select_random_word(word_list)
 print(word)
-guess = input("Enter a single letter: ")
-if len(guess) == 1 and guess.isalpha():
-    print("Good guess!")
-else:
-    print("Oops! That is not a valid input.")
+
+# Prompt user to enter a single letter
+guess = prompt_user_for_guess()
+
+# Validate the user's guess
+validate_guess(guess)
